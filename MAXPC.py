2191b52a248a485ea20bd0e55808f04b492800c5
xdneil <103174787+xdneil@users.noreply.github.com>
2023-05-09 12:49:22 +0800

    5.08.1248

--- MAXPC.py
+++ MAXPC.py
@@ -18,6 +18,41 @@ class Main_Program(QtWidgets.QMainWindow):
     def __init__(self):
         super(Main_Program, self).__init__()
         uic.loadUi('main.ui', self)
+        self.add = add()
+        self.edit = edit()
+        self.restock = restock()
+        self.btnAdd.clicked.connect (lambda: (self.add.show(), self.close()))
+        self.btnEdit.clicked.connect (lambda: (self.edit.show(), self.close()))
+        self.btnRestock.clicked.connect (lambda: (self.restock.show(), self.close()))
+        self.add.btnCancel2.clicked.connect (lambda: (self.add.close(), self.show()))
+        self.edit.btnCancel2.clicked.connect (lambda: (self.edit.close(), self.show()))
+        self.restock.btnCancel3.clicked.connect (lambda: (self.restock.close(), self.show()))
+        
+class add(QtWidgets.QMainWindow,):
+    def __init__(self):
+        super(add, self).__init__()
+        uic.loadUi('add_edit.ui', self)
+        self.lbladd_edit.setText('Add Window')
+
+    def display(self):
+        self.show()
+        
+class edit(QtWidgets.QMainWindow,):
+    def __init__(self):
+        super(edit, self).__init__()
+        uic.loadUi('add_edit.ui', self)
+        self.lbladd_edit.setText('Edit Window')
+
+    def display(self):
+        self.show()
+        
+class restock(QtWidgets.QMainWindow,):
+    def __init__(self):
+        super(restock, self).__init__()
+        uic.loadUi('restock.ui', self)
+
+    def display(self):
+        self.show()
 
 class LogIn (QSplashScreen):
     def __init__(self):
